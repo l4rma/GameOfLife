491fa21dfa237a9a1a8fc8e8bcc126619769e4ee
Lars <lars.magelssen@gmail.com>
2020-05-09 08:49:51 +0200

    #1 Created class simulation

---
+++ src/main/java/org/example/Simulation.java
@@ -0,0 +1,103 @@
+package org.example;
+
+public class Simulation {
+    private int width;
+    private int height;
+    private int[][] board;
+
+    public Simulation(int width, int height) {
+        this.width = width;
+        this.height = height;
+        board = new int[width][height];
+    }
+
+    public void printBoard() {
+        for (int y = 0; y < height; y++) {
+            String line = "| ";
+            for (int x = 0; x < width; x++) {
+                if(this.board[x][y] == 0) {
+                    line += ". ";
+                } else {
+                    line += "x ";
+                }
+            }
+            line += "| ";
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+
+    public int neighbours(int x, int y) {
+        int count = 0;
+        count += getState(x - 1,y - 1);
+        count += getState(x,y - 1);
+        count += getState(x + 1,y - 1);
+
+        count += getState(x - 1,y);
+        count += getState(x + 1,y);
+
+        count += getState(x - 1,y + 1);
+        count += getState(x,y + 1);
+        count += getState(x + 1,y + 1);
+
+        return count;
+    }
+
+    public int getState(int x, int y) {
+        if (x < 0 || x >= width) {
+            return 0;
+        }
+        if (y < 0 || y >= height) {
+            return 0;
+        }
+        return this.board[x][y];
+    }
+
+    public void step() {
+        int[][] nextBoard = new int[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (getState(x, y) == 1) {
+                    if (this.neighbours(x,y) < 2) {
+                        nextBoard[x][y] = 0;
+                    } else if (this.neighbours(x,y) == 2 || this.neighbours(x,y) == 3) {
+                        nextBoard[x][y] = 1;
+                    } else {
+                        nextBoard[x][y] = 0;
+                    }
+                } else if (getState(x, y) == 0) {
+                    if (this.neighbours(x,y) == 3) {
+                        nextBoard[x][y] = 1;
+                    } else {
+                        nextBoard[x][y] = 0;
+                    }
+                }
+            }
+        }
+        this.board = nextBoard;
+    }
+
+    public void fillBoard() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                this.board[x][y] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Simulation grid = new Simulation(7,5);
+        grid.fillBoard();
+
+        grid.board[2][2] = 1;
+        grid.board[3][2] = 1;
+        grid.board[4][2] = 1;
+        grid.printBoard();
+        grid.step();
+        grid.printBoard();
+        grid.step();
+        grid.printBoard();
+        grid.step();
+        grid.printBoard();
+    }
+}
